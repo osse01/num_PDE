f00afe5c651c10aaa1d873dd9c9807a12ca10e05
Oscar <o.jemsson@gmail.com>
2025-05-13 09:33:25 +0200

    Added more functionality

--- lab2_advection_diffusion/main.m
+++ lab2_advection_diffusion/main.m
@@ -42,11 +42,8 @@ for Nx = N
     x = linspace(-1,1,Nx)';
     u_approx = u_0(x);
     % p = plot(x,u_approx);
-    % xlim([-1,1]);
-    % legend('u aproximate','u exact');
-    % title("Wave Plot");
     % drawnow
-    for t = 0:dt:t_end
+    for t = dt:dt:t_end
         u_exact = u_xt(x,t,a,b,50);
         u_approx = updateU(u_approx);
         % p = plot(x,u_approx,x,u_exact);
@@ -57,11 +54,118 @@ for Nx = N
     end
     disp(Nx)
     disp(dx)
-    e = norm(u_approx - u_exact)
+    e = sqrt(dx)*norm(u_approx - u_exact)
 
     err = [err,e];
 end
+
 for k = 2:5
     EOC(k) = log(err(k)/err(k-1)) / log(N(k-1)/N(k));
 end
-plot(N,EOC)
+plot(N(2:5),EOC(2:5),'-x')
+% hold on
+% p = polyfit(log(N(2:5)), log(EOC(2:5)), 1);
+% fitted_line = exp(polyval(p, log(N)));
+% loglog(N, fitted_line, '--','DisplayName', sprintf('Fit (p=%.2f)', p(1)));
+% legend('EOC',sprintf('Fit (p=%.2f)', p(1)))
+xlim([15,450])
+ylim([0,3])
+xlabel('N_x');
+ylabel('EOC');
+title('EOC')
+xticks(N);
+xticklabels(arrayfun(@num2str, N, 'UniformOutput', false));
+
+
+
+%% 2.4
+a = 1;
+b = 0;
+t_end = 2;
+Nx = 300;
+dx = 3/(Nx-1);
+dt = 0.5/250; % 250 time steps until t_end
+u_0 = @(x) exp(-1000*(x-0.5).^2);
+
+% Help variables
+alpha = a * dt / (4*dx);
+beta  = b * dt / (2*dx^2);
+
+% Construct matrix RHS
+main_diag = ones(Nx, 1) * (1 - 2 * beta);
+lower_diag = ones(Nx-1, 1) * (alpha + beta);
+upper_diag = -ones(Nx-1, 1) * (alpha - beta);
+main_diag(1) = 1;
+upper_diag(1) = 0;
+lower_diag(end) = 0;
+main_diag(end) = 1;
+RHS = diag(main_diag) + ...
+      diag(lower_diag, -1) + ...
+      diag(upper_diag, 1);
+
+updateU = @(u) thomasAlg([-(alpha + beta)*ones(1,Nx-2),0], ...
+                        [1,(1+2*beta)*ones(1,Nx-2),1], ...
+                        [0,(alpha-beta)*ones(1,Nx-2)], ...
+                        RHS*u);
+
+x = linspace(0,3,Nx)';
+u_approx = u_0(x);
+p = plot(x,u_approx);
+drawnow
+for t = dt:dt:t_end
+    u_approx = updateU(u_approx);
+    p = plot(x,u_approx,x,u_exact);
+    xlim([0,3]);
+    legend('u aproximate');
+    title("Wave Plot");
+    xlabel('x')
+    ylabel('u')
+    drawnow
+end
+
+%% 2.5
+a = 1;
+%b = 0;
+t_end = 3;
+Nx = 300;
+dx = 1/(Nx-1);
+b = 3*a*dx
+dt = 0.5/250; % 250 time steps until t_end
+u_0 = @(x) x;
+
+% Help variables
+alpha = a * dt / (4*dx);
+beta  = b * dt / (2*dx^2);
+
+
+% Construct matrix RHS
+main_diag = ones(Nx, 1) * (1 - 2 * beta);
+lower_diag = ones(Nx-1, 1) * (alpha + beta);
+upper_diag = -ones(Nx-1, 1) * (alpha - beta);
+main_diag(1) = 1;
+upper_diag(1) = 0;
+lower_diag(end) = 0;
+main_diag(end) = 1;
+RHS = diag(main_diag) + ...
+      diag(lower_diag, -1) + ...
+      diag(upper_diag, 1);
+
+updateU = @(u) thomasAlg([-(alpha + beta)*ones(1,Nx-2),0], ...
+                        [1,(1+2*beta)*ones(1,Nx-2),1], ...
+                        [0,(alpha-beta)*ones(1,Nx-2)], ...
+                        RHS*u);
+
+x = linspace(0,1,Nx)';
+u_approx = u_0(x);
+p = plot(x,u_approx);
+drawnow
+for t = dt:dt:t_end
+    u_approx = updateU(u_approx);
+    p = plot(x,u_approx,x,u_exact);
+    xlim([0,1]);
+    legend('u aproximate');
+    title("Wave Plot");
+    xlabel('x')
+    ylabel('u')
+    drawnow
+end
